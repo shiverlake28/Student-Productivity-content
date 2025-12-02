04c9e7c97267db612316e883ddbd9d49f85649f5
shiverlake28 <adityapasari278@gmail.com>
2025-12-02 13:53:29 +0530

    Arranging items

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./assets/components/Navbar";
 import Profile from "./assets/components/profile";
-import Login from "./assets/pages/login";
-import SignUp from "./assets/pages/SignUp";
+import Login from "./assets/AuthFront-tsx/login";
+import SignUp from "./assets/AuthFront-tsx/SignUp";
 
 function App() {
   return (
